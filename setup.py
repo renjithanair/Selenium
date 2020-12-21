a3b152e69f4b6d07d387ca11120d30e1fbf31177
renjitha <renjithapnair@gmail.com>
2020-12-21 15:06:47 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="selenium-core",  # Replace with your own username
+    version="0.0.1",
+    author="test",
+    author_email="author@example.com",
+    description="basic framework",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
