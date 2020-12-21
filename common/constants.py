24892324052fd1bf4fa5189bb119f3c22a17b0e3
renjitha <renjithapnair@gmail.com>
2020-12-21 15:07:51 +0530

    Create constants.py

---
+++ common/constants.py
@@ -0,0 +1,13 @@
+class Constants(object):
+    DRIVER_CHROME = "chrome"
+    DRIVER_INSTALLATION_PATH = "C:\\Users\\renji\\Downloads\\chromedriver_win32\\chromedriver.exe"
+    NAVIGATION_URL = "http://www.google.com/"
+    # select "https://www.booking.com/searchresults.html?label=gen173nr-1FCAEoggI46AdIM1gEaGyIAQGYATG4ARfIAQzYAQHoAQH4AQKIAgGoAgO4At6E7P4FwAIB0gIkNThiMjJiMjktNmE2Ny00MGI4LWJlOTEtMTIzNDFkMmY4NWI02AIF4AIB&sid=9f3fda83efbeafe3b718e32b303ab8ab&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.html%3Flabel%3Dgen173nr-1FCAEoggI46AdIM1gEaGyIAQGYATG4ARfIAQzYAQHoAQH4AQKIAgGoAgO4At6E7P4FwAIB0gIkNThiMjJiMjktNmE2Ny00MGI4LWJlOTEtMTIzNDFkMmY4NWI02AIF4AIB%3Bsid%3D9f3fda83efbeafe3b718e32b303ab8ab%3Bsb_price_type%3Dtotal%26%3B&ss=Koll%C5%ABru%2C+Karnataka%2C+India&is_ski_area=&checkin_year=2021&checkin_month=1&checkin_monthday=29&checkout_year=2021&checkout_month=1&checkout_monthday=30&group_adults=2&group_children=2&no_rooms=1&age=12&age=12&b_h4u_keep_filters=&from_sf=1&search_pageview_id=1308316f8f7a000d&ac_suggestion_list_length=4&ac_suggestion_theme_list_length=0&ac_position=0&ac_langcode=en&ac_click_type=b&dest_id=-2101302&dest_type=city&place_id_lat=13.8667&place_id_lon=74.800003&search_pageview_id=1308316f8f7a000d&search_selected=true&ss_raw=kollur"
+    # label "https://www.google.com/search?sxsrf=ALeKk031BSYwVG2JqBA5Bl0rT0L6ocTwQw%3A1608186192481&ei=UPnaX_f0HLGW4-EPtdypKA&q=webside+having+++label+element+in+python+selenium&oq=webside+having+++label+element+in+python+selenium&gs_lcp=CgZwc3ktYWIQAzoECAAQR1CKUFj7iQFgyYwBaABwA3gAgAHQAYgB-xaSAQYwLjE2LjKYAQCgAQGqAQdnd3Mtd2l6yAEIwAEB&sclient=psy-ab&ved=0ahUKEwj33PC1sNTtAhUxyzgGHTVuCgUQ4dUDCA0&uact=5"
+    # "http://www.google.com/"
+    # for testing checkbox "https://html.com/input-type-checkbox/"
+
+    NAME = "NAME"
+    ID = "ID"
+    CLASS_NAME = "CLASS_NAME"
+    LINK_TEXT = "LINK_TEXT"
